e1e6253406683ac284b407a0f2a712b699bfd4a4
Student <sibren.eeckhout@student.howest.be>
2024-02-07 13:09:31 +0100

    added .env file

--- HUNTSEASON_BACK/config/database.js
+++ HUNTSEASON_BACK/config/database.js
@@ -2,10 +2,8 @@ import { config } from "https://deno.land/x/dotenv/mod.ts";
 import { Client } from "https://deno.land/x/mysql/mod.ts";
 import { load } from "https://deno.land/std@0.214.0/dotenv/mod.ts";
 import "https://deno.land/x/dotenv/load.ts";
-console.log(Deno.env.get("URL"));
 
-
-console.log(config());
+const env = await load();
 
 const dbConfig = {
   hostname: env["URL"],
@@ -13,8 +11,6 @@ const dbConfig = {
   password: env["PASSWORD"],
   db: env["DATABASE"], // Replace with your database name
 };
-
-console.log(dbConfig);
 
 const client = await new Client().connect({
     hostname: dbConfig.hostname,
