ec8645a84258f39a51f42382106935600fe4405a
Student <sibren.eeckhout@student.howest.be>
2024-02-20 19:00:32 +0100

    small refactor

--- HUNTSEASON_BACK/controllers/userController.js
+++ HUNTSEASON_BACK/controllers/userController.js
@@ -1,11 +1,11 @@
-import { getUsersFromDatabase, insertUser, getUserById } from "../services/userService.js";
+import UserService from "../services/userService.js";
 import {User} from "../models/userModal.js";
 import jwt from 'jsonwebtoken';
 
 class UserController {
   static async getUsers(req, res) {
     try {
-      const users = await getUsersFromDatabase();
+      const users = await UserService.getUsersFromDatabase();
       res.json(users);
     } catch (error) {
       res.status(500).json({ message: error.message });
@@ -15,7 +15,7 @@ class UserController {
   static async getUserById(req, res) {
     try {
       const userId = req.params.id;
-      const user = await getUserById(userId);
+      const user = await UserService.getUserById(userId);
       console.log(user);
       res.json(user);
     } catch (error) {
@@ -28,12 +28,10 @@ class UserController {
       const { name, number, email, password } = req.body;
 
       const newUser = new User(null, name, number, email, password); // Pass plain text password
-      const insertedUser = await insertUser(newUser.name, newUser.number, newUser.email, newUser.password);
+      const insertedUser = await UserService.insertUser(newUser.name, newUser.number, newUser.email, newUser.password);
 
       // Generate a JWT token for the newly registered user
-      console.log("ets");
       const token = UserController.generateToken(insertedUser.id);
-      console.log("asdf");
       // Respond with the inserted user and the JWT token
       res.status(201).json({ user: insertedUser, token });
     } catch (error) {
--- HUNTSEASON_BACK/services/userService.js
+++ HUNTSEASON_BACK/services/userService.js
@@ -1,40 +1,41 @@
-// services/userService.js
 import client from "../config/database.js";
 
-const getUsersFromDatabase = async () => {
-  try {
-    const results = await client.query("SELECT * FROM users");
-    console.log(results);
-    return results; // Assuming 'rows' contains the data
-  } catch (error) {
-    throw new Error("Error retrieving users from the database");
+class UserService {
+  static async getUsersFromDatabase() {
+    try {
+      const results = await client.query("SELECT * FROM users");
+      console.log(results);
+      return results; // Assuming 'rows' contains the data
+    } catch (error) {
+      throw new Error("Error retrieving users from the database");
+    }
   }
-};
 
-const insertUser = async (name, number, email, password) => {
-  try {
-    const query = `
-      INSERT INTO users (name, number, email, password)
-      VALUES ('${name}', '${number}', '${email}', '${password}')`;
-    const result = await client.query(query);
-    return "Inserted correctly.";
-  } catch (error) {
-    console.error("Error inserting user into the database:", error);
-    throw new Error("Error inserting user into the database");
+  static async insertUser(name, number, email, password) {
+    try {
+      const query = `
+        INSERT INTO users (name, number, email, password)
+        VALUES ('${name}', '${number}', '${email}', '${password}')`;
+      const result = await client.query(query);
+      return "Inserted correctly.";
+    } catch (error) {
+      console.error("Error inserting user into the database:", error);
+      throw new Error("Error inserting user into the database");
+    }
   }
-};
 
-const getUserById = async (userId) => {
-  try {
-    const query = `
-      SELECT * FROM users
-      WHERE id = ${userId}`;
-    const result = await client.query(query);
+  static async getUserById(userId) {
+    try {
+      const query = `
+        SELECT * FROM users
+        WHERE id = ${userId}`;
+      const result = await client.query(query);
 
-    return result; // Return the first user found (assuming ID is unique)
-  } catch (error) {
-    throw new Error(`Error retrieving user by ID: ${error.message}`);
+      return result; // Return the first user found (assuming ID is unique)
+    } catch (error) {
+      throw new Error(`Error retrieving user by ID: ${error.message}`);
+    }
   }
-};
+}
 
-export { getUsersFromDatabase, insertUser, getUserById };
+export default UserService;
